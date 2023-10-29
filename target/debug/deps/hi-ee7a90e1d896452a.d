7ec1da2094fa12c6619a0cbdf004a82802042fde
int0p <closer.fz@gmail.com>
2023-10-29 13:03:44 +0900

    Remove previously tracked target folder

--- target/debug/deps/hi-ee7a90e1d896452a.d
+++
@@ -1,5 +0,0 @@
-/Users/gimseung-ye/Projects/rust-web-study/rust-axum-course/target/debug/deps/hi-ee7a90e1d896452a: src/main.rs
-
-/Users/gimseung-ye/Projects/rust-web-study/rust-axum-course/target/debug/deps/hi-ee7a90e1d896452a.d: src/main.rs
-
-src/main.rs:
--- target/debug/hi.d
+++
@@ -1,1 +0,0 @@
-/Users/gimseung-ye/Projects/rust-web-study/rust-axum-course/target/debug/hi: /Users/gimseung-ye/Projects/rust-web-study/rust-axum-course/src/main.rs
