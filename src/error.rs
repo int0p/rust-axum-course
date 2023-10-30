b1d8e81d01988e003fb9cf3af6defe09993a97bb
int0p <closer.fz@gmail.com>
2023-10-30 20:05:31 +0900

    run api request only when login request successed

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,9 @@ pub type Result<T> = core::result::Result<T,Error>;
 pub enum Error{
     LoginFail,
 
+    // -- Auth errors
+    AuthFailNoAuthTokenCookie,
+
     // -- Model errors
     TicketDeleteFailIdNotFound {id: u64},
 }
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -9,20 +9,20 @@ async fn quick_dev() -> Result<()> {
 
     // hc.do_get("/hello").await?.print().await?;
 
-    // hc.do_get("/hello?name=Jen").await?.print().await?;
+    hc.do_get("/hello?name=Jen").await?.print().await?;
 
     // hc.do_get("/hello2/Mike").await?.print().await?;
 
     // hc.do_get("/src/main.rs").await?.print().await?;
 
-    // let req_login = hc.do_post(
-    //     "/api/login",
-    //     json!({
-    //         "username":"demo1",
-    //         "pwd":"welcome"
-    //     })
-    // );
-    // req_login.await?.print().await?;
+    let req_login = hc.do_post(
+        "/api/login",
+        json!({
+            "username":"demo1",
+            "pwd":"welcome"
+        })
+    );
+    // req_login.await?.print().await?; //얘 없으면 api요청 불가능. 
 
     let req_create_ticket = hc.do_post(
         "/api/tickets",
